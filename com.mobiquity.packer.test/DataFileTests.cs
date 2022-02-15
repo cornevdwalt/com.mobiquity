ac36d9d966c5393ae77aa5b32e006f3e83d337bf
Corne van der Walt <cornevdwalt@gmail.com>
2022-02-15 10:27:23 +0200

    Added test cases for DataFile and DataLine unit testing

--- com.mobiquity.packer.test/DataFileTests.cs
+++ com.mobiquity.packer.test/DataFileTests.cs
@@ -8,52 +8,33 @@ namespace com.mobiquity.packer.test
         public void DataFileIsNotEmpty()
         {
             // Arrange
+            // TODO - call service to retrieve the content of the data file
+            string dataFileContent = string.Empty;
+
 
             // Act
+            bool hasContent = dataFileContent.Length > 0;
+
 
             // Assert
+            Assert.True(hasContent, "The datafile needs to contains information");
         }
 
         [Fact]
         public void DataFileParseSuccessfully()
         {
             // Arrange
+            // TODO - Call service to parse file and confirm all data structures were successfully created
+            bool parseDataFile = false;
 
             // Act
+            bool hasParseSuccesfully = parseDataFile;
+
+
+            // Assert
+            Assert.True(hasParseSuccesfully, "The datafile needs to contains well formatted data to parse succesfully");
 
             // Assert
         }
-
-
-        [Fact]
-        public void NumberOfItemsinDataLineLessEqualTo15()
-        {
-            // Arrange
-
-            // Act
-
-            // Assert
-        }
-
-        [Fact]
-        public void DataLineTotalWeightGreaterThanZero()
-        {
-            // Arrange
-
-            // Act
-
-            // Assert
-        }
-
-        [Fact]
-        public void DataLineHasItem()
-        {
-            // Arrange
-
-            // Act
-
-            // Assert
-        }
-
     }
 }
--- com.mobiquity.packer.test/DataLineTests.cs
+++ com.mobiquity.packer.test/DataLineTests.cs
@@ -4,34 +4,92 @@ namespace com.mobiquity.packer.test
 {
     public class DataLineTests
     {
+        int lineNumber = 0;
+
+        [Fact]
+        public void DataLineTotalWeightGreaterThanZero()
+        {
+            // Arrange
+            lineNumber = 10;
+
+            // Act
+            bool totalPackageWeightGreaterThanZero = false;
+
+            // Assert
+            Assert.True(totalPackageWeightGreaterThanZero, $"The total weight of a package allowed must be greater than zero. Line# {lineNumber}");
+        }
+
+        [Fact]
+        public void DataLineHasAtLeastOneItem()
+        {
+            // Arrange
+            lineNumber = 10;
+            int numberOfItemsInLine = 0;
+
+            // Act
+            bool atLeastOneItemInLine = numberOfItemsInLine > 0;
+
+            // Assert
+            Assert.True(atLeastOneItemInLine, $"There must be at least one item in a package. Line# {lineNumber}");
+        }
+
+        [Fact]
+        public void NumberOfItemsinDataLineLessEqualTo15()
+        {
+            // Arrange
+            lineNumber = 10;
+            int numberOfItemsInLine = 20;
+
+            // Act
+            bool itemsInRange = numberOfItemsInLine <= 15;
+
+            // Assert
+            Assert.True(itemsInRange, $"There cannot be more than 15 items in a single package. Line# {lineNumber}");
+        }
+
         [Fact]
         public void ItemWeightLessEqualTo100()
         {
             // Arrange
+            lineNumber = 10;
+            int itemWeight = 200;
+            int allowedWeight = 100;
 
             // Act
+            bool itemCorrectWeight = itemWeight <= allowedWeight;
 
             // Assert
+            Assert.True(itemCorrectWeight, $"An item may not weight more then {allowedWeight}. Line# {lineNumber}");
         }
 
         [Fact]
         public void ItemCostLessEqualTo100()
         {
             // Arrange
+            lineNumber = 10;
+            int itemCost = 200;
+            int allowedCost = 100;
 
             // Act
+            bool itemCorrectCost = itemCost <= allowedCost;
 
             // Assert
+            Assert.True(itemCorrectCost, $"The cost of an item may not be more then {allowedCost}. Line# {lineNumber}");
         }
 
         [Fact]
         public void TotalPackageWeightLessEqualTo100()
         {
             // Arrange
+            lineNumber = 10;
+            int totalPackageWeight = 200;
+            int allowedWeight = 100;
 
             // Act
+            bool packageCorrectWeight = totalPackageWeight <= allowedWeight;
 
             // Assert
+            Assert.True(packageCorrectWeight, $"The total weight of a package may not be more then {allowedWeight}. Line# {lineNumber}");
         }
     }
 }
